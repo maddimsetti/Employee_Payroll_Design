bb2af050c17c6e4b94a6dc621ba7fe9242d17bf7
prudhvi <prudhvieethakota123@gmail.com>
2021-06-13 23:48:57 +0530

    Added: Code for Modify Employpee Payroll Class

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,81 @@
+/**
+ * @description Created Class for Employee PayRoll Data
+ **/
+ class EmployeePayrollData {
+     
+    /**
+     * @description Created Getter and Setter Method
+     **/
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-zA-Z\\s]{2,}$");
+        if (nameRegex.test(name)){
+            this._name = name;
+        } else {
+            throw "Name is Incorrect";
+
+        }
+    }
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    get profilePic() {
+        return this._profilePic;
+    }
+    set profilePic(profilePic) {
+        return this._profilePic = profilePic; 
+    }
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+    }
+
+    get note() {
+        return this._note;
+    }
+
+    set note(note) {
+        this._note = note;
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        this._startDate = startDate;
+    }
+
+    //method
+    toString() {
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const empDate = !this.startDate ? "undefined" :
+                        this.startDate.toLocaleDateString("en-US",options);
+        return "id = " +this.id+ ", name = " +this.name+ ", gender = " +gender+ ", ProfilePic = " +this.profilePic+ ", department = " +this._department+ ", salary = " +this.salary+ 
+                     ", Start Date = " +empDate+ ", note = " +this.note;
+    }
+}
